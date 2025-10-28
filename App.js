1c1756d71b0b7e3d1c44ae2697aa764948916a91
Mark <mark.loyola627@gmail.com>
2025-10-28 16:14:48 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar, Appearance, useColorScheme, View, StyleSheet, ScrollView, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './Home';
+import HomeSignIn from './HomeSignIn'
 
-export default function App() {
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Ionicons } from "@expo/vector-icons";
+import Search from './SearchPage';
+import BottomTabBar from './BottomTabBar';
+
+const Stack = createNativeStackNavigator();
+
+
+// Main App UI
+export default function App () {
+  const colorScheme = useColorScheme();
+
+  const statusBarStyle = colorScheme === 'dark' ? 'light-content' : 'dark-content';
+
   return (
-    <View style={styles.container}>
-      <Text>Testing 1234!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar barStyle={statusBarStyle} />
+      {/* 3. Define the stack navigator and screens */}
+      <Stack.Navigator>
+        {/* Set Home as the initial screen and hide its header */}
+        <Stack.Screen
+        name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        {/* Register the HomeSignIn screen */}
+        <Stack.Screen
+          name="HomeSignIn"
+          component={HomeSignIn}
+          options={{ headerShown: false}} // Remove back button from top left bar (Ex: <Home (shown on top left bar))
+        />
+      </Stack.Navigator>
+      
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
+
+  ScrollView: {
+    flex: 1,
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    alignSelf: 'center',
     justifyContent: 'center',
+    padding: 10,
   },
 });
